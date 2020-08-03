4b9bea4db342dad526b845570242e55aeda43f01
Mariia <mytsko.mariia@gmail.com>
2020-08-03 20:31:44 +0200

    correcting

--- main.js
+++ main.js
@@ -21,48 +21,11 @@ const getNumber = (min, max) => {
 
 console.log(`Function#1,random array:`, getRandomArray(20, getNumber));
 
-// //function#2
-// const getModa = (...numbers) => {
-//   let arr = [];
-//   let countMax = 0;
-//   let count = 0;
-//   let numb = 0;
-//   let i = 0;
-//   arr = numbers;
-//   console.log(arr);
-//   for (arr[i] of arr) {
-//     numb = arr[i];
-//     console.log(`arr[i]`, arr[i]);
-//     if (numb === arr[i]) {
-//       count++;
-//     } else {
-//       count;
-//     }
-//     console.log(`numb`, numb);
-//     console.log(`count`, count);
-//     if (count > countMax) {
-//       countMax = count;
-//       console.log(`countMax`, countMax);
-//     }
-//   }
-//   //   arr.forEach(function (el) {
-//   //     if (el == el) {
-//   //       count += 1;
-//   //     }
-//   //   });
-
-//   //   console.log(`count`, countMax);
-// };
-// //return countMax;
-
-// console.log(getModa(3, 6, 2, 4, 3, 2, 6, 2, 5, 9, 2));
-
 //function#3
 const getAvarage = (...numbers) => {
   let arr = [];
   let count = 0;
   let sum = 0;
-  let i = 0;
   arr = numbers;
 
   arr.forEach(function (el) {
@@ -72,9 +35,9 @@ const getAvarage = (...numbers) => {
     }
   });
 
-  return console.log(`Function #3.Avvarage of numbers :`, sum / count);
+  return console.log(`Function #3.Avarage of numbers :`, sum / count);
 };
-getAvarage(3, -3, 5, 9, 3, 5.6, 7, 1);
+getAvarage(1, -1, 9, 4.3, 8, 3, -2, 4, 5.6);
 
 //function#4
 
